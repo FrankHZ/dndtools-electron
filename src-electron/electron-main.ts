5a466a85285db5cb0f68d700c2a27381574863d1
Hongzhou Fang <hf92@drexel.edu>
2024-07-15 23:06:32 -0400

    Added and updated some dependencies

--- src-electron/electron-main.ts
+++ src-electron/electron-main.ts
@@ -1,13 +1,14 @@
 import { app, BrowserWindow } from 'electron';
 import path from 'path';
 import os from 'os';
+import installExtension, { VUEJS_DEVTOOLS } from "electron-devtools-installer";
 
 // needed in case process is undefined under Linux
 const platform = process.platform || os.platform();
 
 let mainWindow: BrowserWindow | undefined;
 
-function createWindow() {
+function createWindow () {
   /**
    * Initial window options
    */
@@ -28,6 +29,9 @@ function createWindow() {
   if (process.env.DEBUGGING) {
     // if on DEV or Production with debug enabled
     mainWindow.webContents.openDevTools();
+    installExtension(VUEJS_DEVTOOLS)
+      .then((name) => console.log(`Added Extension:  ${name}`))
+      .catch((err) => console.log("An error occurred: ", err));
   } else {
     // we're on production; no access to devtools pls
     mainWindow.webContents.on('devtools-opened', () => {
